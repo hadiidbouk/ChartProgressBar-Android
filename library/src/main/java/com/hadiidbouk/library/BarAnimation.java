9d915f7a5b0d7ec776e14abc91a371bd79afc4fe
Hadi <hdbouk@simplexity.io>
2017-09-11 16:47:37 +0300

    Adding bar Animation

---
+++ library/src/main/java/com/hadiidbouk/library/BarAnimation.java
@@ -0,0 +1,26 @@
+package com.hadiidbouk.library;
+
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ProgressBar;
+
+public class BarAnimation extends Animation {
+	private ProgressBar progressBar;
+	private float from;
+	private float  to;
+
+	public BarAnimation(ProgressBar progressBar, float from, float to) {
+		super();
+		this.progressBar = progressBar;
+		this.from = from;
+		this.to = to;
+	}
+
+	@Override
+	protected void applyTransformation(float interpolatedTime, Transformation t) {
+		super.applyTransformation(interpolatedTime, t);
+		float value = from + (to - from) * interpolatedTime;
+		progressBar.setProgress((int) value);
+	}
+
+}
